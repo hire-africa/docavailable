0a130d126b0ca0cab5b5317f0bac030fde7e0353
sleeky <161637665+sleekyb@users.noreply.github.com>
2025-10-30 02:25:58 +0200

    Register IncomingCallPackage for incoming call module

--- android/app/src/main/java/com/docavailable/app/MainApplication.kt
+++ android/app/src/main/java/com/docavailable/app/MainApplication.kt
@@ -23,8 +23,8 @@ class MainApplication : Application(), ReactApplication {
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
             val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(MyReactNativePackage())
+            // Register custom packages that are not autolinked
+            packages.add(IncomingCallPackage())
             return packages
           }
 
